8f4b359818b9e0be6e21884ab301e50d01205b16
zhibo-yu <74233111+zhibo-yu@users.noreply.github.com>
2026-09-22 16:30:03 +0000

    Deploying to gh-pages from @ zhibo-yu/zhibo-yu.github.io@0386591ee6351044899970befe42ee3ba32c93a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/contact/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/cv/";
-          },
         },{id: "nav-my-photography",
           title: "My Photography",
           description: "During my spare time, I like travel and taking photos along the way. I also claim myself as an amateur astrophotographer, and a dedicated eclipse chaser!",
